e45d01f7107a531688b0ff01d3894ba9230b786a
alex <programmingwithalex3@gmail.com>
2026-08-10 23:33:52 -0400

    fix: format immutable ECR configuration

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,8 +1,8 @@
 resource "aws_ecr_repository" "application" {
   for_each = var.ecr_repository_names
 
-  name                 = each.value
-  force_delete         = true
+  name         = each.value
+  force_delete = true
   # prevent a commit SHA tag from being overwritten with different image contents
   image_tag_mutability = "IMMUTABLE"
 
